1304510e248d272119d8da6b1600edda65596838
Mostafa Elbannan <mostafaelbannan1@gmail.com>
2020-09-23 13:55:55 -0600

    removed NN directory

--- neural_network/networks/model_predict.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-string = tf.constant(['I love amazon echo'], dtype=tf.string)
-
-new_model = tf.keras.models.load_model('my_model')
-
-print(new_model.predict(string)[0][0])
